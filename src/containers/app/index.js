8bbc47181ca46ad9dfb720c7d0a2654b8f864486
rickithadi <rickithadi@gmail.com>
2018-11-03 20:07:25 +0800

    accidentally removed main, laze

--- src/containers/app/index.js
+++ src/containers/app/index.js
@@ -17,7 +17,7 @@ const App = () => (
         </NavLink>
       </Menu.Item>
       <Menu.Item key="/about">
-        <NavLink to="/about-anus">
+        <NavLink to="/about-us">
           <Icon type="about" />
           <span>about</span>
         </NavLink>
@@ -32,49 +32,15 @@ const App = () => (
     </Menu>
 
 
-    {/* <header> */}
-    {/*   <Link to="/">Home</Link> */}
-    {/*   <Link to="/about-us">About</Link> */}
-    {/*   <Link to="/vagene">show bob</Link> */}
-    {/* </header> */}
-
-    {/* <Sider className="sidebar" width={280} style={{ background: '#fafafa' }}> */}
-    {/*   <Menu className="sidebar" */}
-    {/*   style={{ width: 280, background: '#fafafa', height: '90vh' }} */}
-    {/*   defaultSelectedKeys={['1']} */}
-    {/*   defaultOpenKeys={['sub1']} */}
-    {/*   mode="inline"> */}
-    {/*     <Menu.Item className="sidebar-item" key="1"> */}
-    {/*       <Link to="/"> */}
-    {/*         <Icon type="appstore" /> */}
-    {/*         OVERVIEW */}
-    {/*       </Link> */}
-    {/*     </Menu.Item> */}
-    {/*     <Menu.Item className="sidebar-item" key="2"> */}
-    {/*       <Link to="/issues"> */}
-    {/*         <Icon type="bars" /> */}
-    {/*         ISSUES */}
-    {/*       </Link> */}
-    {/*     </Menu.Item> */}
-    {/*     <Menu.Item className="sidebar-item" key="3"> */}
-    {/*       <Link to="/calendar"> */}
-    {/*         <Icon type="calendar" /> */}
-    {/*         CALENDAR */}
-    {/*       </Link> */}
-    {/*     </Menu.Item> */}
-    {/*     <Menu.Item className="sidebar-item" key="4"> */}
-    {/*       <Link to="/settings"> */}
-    {/*         <Icon type="setting" /> */}
-    {/*         SETTINGS */}
-    {/*       </Link> */}
-    {/*     </Menu.Item> */}
-    {/*   </Menu> */}
-    {/* </Sider> */}
-    {/* <main> */}
-    {/*   <Route exact path="/" component={Home} /> */}
-    {/*   <Route exact path="/about-anus" component={About} /> */}
-    {/*   <Route exact path="/vagene" component={About} /> */}
-    {/* </main> */}
+    <header>
+      <Link to="/">Home</Link>
+      <Link to="/about-us">About</Link>
+      <Link to="/vagene">show bob</Link>
+    </header>
+    <main>
+      <Route exact path="/" component={Home} />
+      <Route exact path="/about-us" component={About} />
+    </main>
   </div>
 )
 
